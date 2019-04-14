9803d33487ac65c5792a22d6fd62f5d1bbf276a3
nd <arxnday@gmail.com>
2019-04-14 10:56:39 +0300

    updated script

--- script.js
+++ script.js
@@ -15,22 +15,22 @@ const values = {
     'calculated': false
 };
 
-calculator.addEventListener('click', (e) => {
+calculator.addEventListener('click', (e) => { //отлавливаем событие клика по таблице
     e.preventDefault();
-    let res = e.target.textContent;
+    let res = e.target.textContent; //содержимое ячейки
     console.log(res);
 
-    if ((res == '+') || (res == '-') || (res == '/') || (res == '*') || (res == '=')) {
-        if ((res != '=')) {
+    if ((res == '+') || (res == '-') || (res == '/') || (res == '*') || (res == '=')) { //проверка нажатия кнопки операции
+        if ((res != '=')) { //если не равно - меняем значение аттрибута объекта для вычислений на символ операции и выводим символ на страницу 
             opClicked = true;
             result.value = res;
             values.operation = res;
 
-            if (values.calculated == true) {
+            if (values.calculated == true) { //по идее должно присвоить результат вычислений объекта values.first и продолжить вычисления с этого места, но не работает
                 values.first = res;
             }
-            
-        } else {
+
+        } else { //если нажато равно - вызываем функцию в которой происходят все вычисления, после чего сбрасываем объект до начального состояния
             result.value = calcResults();
             values.first = '';
             values.operation = '';
@@ -39,10 +39,10 @@ calculator.addEventListener('click', (e) => {
             opClicked = false;
         }
 
-    } else if (!opClicked) {
+    } else if (!opClicked) { // если opClicked == false, то записываем значения в левый операнд
         values.first += res;
         result.value = values.first;
-    } else {
+    } else { // а если true - то в правый
         values.second += res;
         result.value = values.second;
     }
